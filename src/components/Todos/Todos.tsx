5740dd8b2d3dd7ca9a4750b8f814434a9fe9c411
virkann <virkan90@gmail.com>
2023-08-18 12:53:54 +0300

    new commit

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { ITodo } from './types'
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { RootState } from '../../store/store'
+
+
+
 
 const todos = () => {
+
+    const todos: ITodo[] = useAppSelector ((state: RootState) => state.todos);
+    const dispatch = useAppDispatch();
     return (
       
       <div>
-          
+          <h1>Todo List</h1>
     </div>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../reducers/counterSlice";
+import todos from "../components/Todos/Todos";
+import todoSlice from "../reducers/todoSlice";
 
 export const store = configureStore({
   reducer: {
     counterSlice: counterSlice,
+    todos: todoSlice,
   },
 });
 
