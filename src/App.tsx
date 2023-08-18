be878dd7067d51704598ab9230e0c6bc8e764949
virkann <virkan90@gmail.com>
2023-08-18 09:10:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { decrement, increment } from "./reducers/counter";
+import { RootState } from "./store/store";
+import { useSelector, useDispatch } from "react-redux";
+
+
+function App() {
+   const count = useSelector((state: RootState) => state.counter.value);
+   const dispatch = useDispatch();
+  return (
+    <>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
